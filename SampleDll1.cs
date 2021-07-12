c1366bba70603758a5f942377937286dec313838
DattatreyaPatil <datts.ptl01@gmail.com>
2021-07-12 22:19:25 +0200

    SAmple dll

---
+++ SampleDll1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstSampleDll
+{
+    using System;
+
+    public class SampleDll1
+    {
+        public void Output(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public string SomeFunction()
+        {
+            return $"FirstSampleDll.SampleDll1.SomeFunction was called/excuted on {DateTime.Now} ";
+        }
+    }
+}
